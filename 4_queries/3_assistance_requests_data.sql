db45835650121b36dc95f73b0520e14392c19305
Max Li <hope_max@outlook.com>
2021-10-02 14:01:28 -0400

    Use LEFT JOIN instead of INNER JOIN

--- 4_queries/3_assistance_requests_data.sql
+++ 4_queries/3_assistance_requests_data.sql
@@ -2,5 +2,5 @@ SELECT teachers.name as teacher, students.name as student, assignments.name as a
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
-JOIN assignments ON assignments.id = assignment_id
+LEFT JOIN assignments ON assignments.id = assignment_id
 ORDER BY duration;
