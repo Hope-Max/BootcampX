fe0224a9f9adb3707eaf44c6f7f392f3128603df
Max Li <hope_max@outlook.com>
2021-10-11 17:56:45 -0400

    Use parameterized query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,15 +8,18 @@ const pool = new Pool({
 });
 
 const queryTeacher = function(cohort) {
-  pool.query(`
+  const querySring = `
   SELECT DISTINCT(teachers.name) as teacher, cohorts.name as cohort
   FROM teachers
   JOIN assistance_requests ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = '${cohort}'
+  WHERE cohorts.name = $1
   ORDER BY teachers.name;
-  `)
+  `;
+  const values = [cohort];
+
+  pool.query(querySring, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort}: ${user.teacher}`);
